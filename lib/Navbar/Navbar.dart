2a0c57da417342344249864096eb3bb1c575c66a
Korhal <ninj4Noob@hotmail.com>
2020-11-04 14:17:31 -0300

    First look

---
+++ lib/Navbar/Navbar.dart
@@ -0,0 +1,140 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class Navbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 1200) {
+          return DesktopNavbar();
+        } else if (constraints.maxWidth > 800 && constraints.maxWidth < 1200) {
+          return DesktopNavbar();
+        } else {
+          return MobileNavbar();
+        }
+      },
+    );
+  }
+}
+
+class DesktopNavbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 0),
+      child: Container(
+        color: Colors.black,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Row(
+                children: <Widget>[
+                  SizedBox(
+                    width: 30,
+                    height: 70,
+                  ),
+                  Image(
+                    image: AssetImage("../assets/images/icon.png"),
+                    width: 56,
+                    height: 56,
+                  ),
+                  SizedBox(
+                    width: 25,
+                  ),
+                  Text(
+                    "Intranet",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 30),
+                  ),
+                ]
+            ),
+            Row(
+              children: <Widget>[
+                Text(
+                  "Home",
+                  style: TextStyle(color: Colors.white),
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                Text(
+                  "About Us",
+                  style: TextStyle(color: Colors.white),
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                Text(
+                  "Portfolio",
+                  style: TextStyle(color: Colors.white),
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                MaterialButton(
+                  color: Colors.pink,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                  onPressed: () {},
+                  child: Text(
+                    "Get Started",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                SizedBox(width: 30)
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MobileNavbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+      child: Container(
+        child: Column(children: <Widget>[
+          Text(
+            "RetroPortal Studio",
+            style: TextStyle(
+                fontWeight: FontWeight.bold, color: Colors.white, fontSize: 30),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Home",
+                  style: TextStyle(color: Colors.white),
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                Text(
+                  "About Us",
+                  style: TextStyle(color: Colors.white),
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                Text(
+                  "Portfolio",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+}
